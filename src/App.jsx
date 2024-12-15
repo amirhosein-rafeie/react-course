d5a0052c641837495470585be70baa35e2ece067
rafeieamirhosein <rafeieamirhosein@gmail.com>
2024-12-15 23:45:07 +0330

    React Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Welcome To Jetino React Course</h1>;
+  const [value, setValue] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+
+  const changeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const clickHandler = () => {
+    setShowMessage((prev) => !prev);
+  };
+
+  const clearHandler = () => {
+    setValue("");
+    setShowMessage(false);
+  };
+
+  return (
+    <div>
+      <input value={value} onChange={changeHandler} />
+      <button onClick={clickHandler}>Show message</button>
+      <button onClick={clearHandler}>Clear</button>
+      {showMessage && <p>{value}</p>}
+    </div>
+  );
 }
 
 export default App;
