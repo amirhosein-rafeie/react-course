c0296dbb1f6fd8ab3551f6c62425562c1a2f12d0
rafeieamirhosein <rafeieamirhosein@gmail.com>
2024-12-26 14:27:06 +0330

    Add App CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import "./App.css";
+
 function App() {
-  return <h1>Welcome To Jetino React Course</h1>;
+  return (
+    <div className="container">
+      <h1>Welcome To Jetino React Course</h1>
+    </div>
+  );
 }
 
 export default App;
