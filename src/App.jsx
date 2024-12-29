6562c674938c5c153e54d75a3df43a634716f014
rafeieamirhosein <rafeieamirhosein@gmail.com>
2024-12-29 22:23:55 +0330

    Add UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome To Jetino React Course</h1>
+    <div className="calculator">
+      <div className="display">0</div>
+      <div className="buttons">
+        {/* Row 1  */}
+        <button className="clear">C</button>
+        <button className="del">DEL</button>
+        <button className="operator">%</button>
+        <button className="operator">÷</button>
+        {/* Row 2  */}
+        <button className="number">7</button>
+        <button className="number">8</button>
+        <button className="number">9</button>
+        <button className="operator">×</button>
+        {/* Row 3  */}
+        <button className="number">4</button>
+        <button className="number">5</button>
+        <button className="number">6</button>
+        <button className="operator">-</button>
+        {/* Row 4  */}
+        <button className="number">1</button>
+        <button className="number">2</button>
+        <button className="number">3</button>
+        <button className="operator">+</button>
+        {/* Row 5  */}
+        <button className="number" style={{ gridColumn: "span 2" }}>
+          0
+        </button>
+        <button className="number">.</button>
+        <button className="operator equal">=</button>
+      </div>
     </div>
   );
 }
