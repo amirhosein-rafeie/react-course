aaf50f0794c0498e1ac7defe1fc3e4d5654e5285
rafeieamirhosein <rafeieamirhosein@gmail.com>
2025-02-04 23:57:47 +0330

    Complete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+const phoneRegex = /^09[0-9]{9}$/;
+const dateRegex = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+
 function App() {
   const [formData, setFormData] = useState({
     firstName: "",
@@ -16,6 +21,60 @@ function App() {
     terms: false,
   });
 
+  const [errors, setErrors] = useState({});
+
+  const vlidateForm = () => {
+    let errors = {};
+
+    if (!formData.firstName.trim()) {
+      errors.firstName = "نام الزامی است";
+    }
+
+    if (!formData.lastName.trim()) {
+      errors.lastName = "نام خانوادگی الزامی است";
+    }
+
+    if (!emailRegex.test(formData.email)) {
+      errors.email = "فرمت ایمیل نامعتبر است";
+    }
+
+    if (!phoneRegex.test(formData.phone)) {
+      errors.phone = "فرمت شماره تلفن نامعتبر است";
+    }
+
+    if (!passwordRegex.test(formData.password)) {
+      errors.password =
+        "در رمزعبور از عدد و کاراکتر استفاده کنید. طول رمز عبور باید 6 باشد.";
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      errors.confirmPassword = "رمزها باید یکسان باشند";
+    }
+
+    if (!dateRegex.test(formData.birthDate)) {
+      errors.birthDate = "فرمت تاریخ نامعتبر است";
+    }
+
+    if (!formData.gender) {
+      errors.gender = "جنسیت الزامی است";
+    }
+
+    if (!formData.country) {
+      errors.country = "کشور الزامی است";
+    }
+
+    if (!formData.terms) {
+      errors.terms = "لطفا با قوانین موافقت کنید";
+    }
+
+    if (formData.bio && formData.bio.length < 4) {
+      errors.bio = "حداقل 3 کاراکتر وارد کنید";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
   const handleChange = (e) => {
     const { value, name, type, checked } = e.target;
     setFormData((prevFormData) => ({
@@ -26,7 +85,9 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault(); // ** important **
-    alert(`Form sent: ${JSON.stringify(formData)}`);
+    if (vlidateForm()) {
+      alert(`Form sent: ${JSON.stringify(formData)}`);
+    }
   };
 
   return (
@@ -35,29 +96,35 @@ function App() {
         <h2>فرم ثبت‌نام</h2>
         <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <input
-              type="text"
-              name="firstName"
-              placeholder="نام"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder="نام خانوادگی"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
+            <div>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="نام"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.firstName}</p>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="نام خانوادگی"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.lastName}</p>
+            </div>
           </div>
 
           <input
-            type="email"
             name="email"
             placeholder="ایمیل"
             value={formData.email}
             onChange={handleChange}
           />
+          <p className="error">{errors.email}</p>
 
           <input
             type="text"
@@ -66,8 +133,9 @@ function App() {
             value={formData.phone}
             onChange={handleChange}
           />
+          <p className="error">{errors.phone}</p>
 
-          <div className="input-group">
+          <div>
             <input
               type="password"
               name="password"
@@ -75,6 +143,9 @@ function App() {
               value={formData.password}
               onChange={handleChange}
             />
+            <p className="error">{errors.password}</p>
+          </div>
+          <div>
             <input
               type="password"
               name="confirmPassword"
@@ -82,30 +153,34 @@ function App() {
               value={formData.confirmPassword}
               onChange={handleChange}
             />
+            <p className="error">{errors.confirmPassword}</p>
           </div>
 
-          <div className="radio-group">
-            <p>جنسیت:</p>
-            <input
-              id="male"
-              type="radio"
-              name="gender"
-              value="male"
-              checked={formData.gender === "male"}
-              onChange={handleChange}
-              className="radio-input"
-            />
-            <label htmlFor="male">مرد</label>
-            <input
-              id="female"
-              type="radio"
-              name="gender"
-              value="female"
-              checked={formData.gender === "female"}
-              onChange={handleChange}
-              className="radio-input"
-            />
-            <label htmlFor="female">زن</label>
+          <div>
+            <div className="radio-group">
+              <p>جنسیت:</p>
+              <input
+                id="male"
+                type="radio"
+                name="gender"
+                value="male"
+                checked={formData.gender === "male"}
+                onChange={handleChange}
+                className="radio-input"
+              />
+              <label htmlFor="male">مرد</label>
+              <input
+                id="female"
+                type="radio"
+                name="gender"
+                value="female"
+                checked={formData.gender === "female"}
+                onChange={handleChange}
+                className="radio-input"
+              />
+              <label htmlFor="female">زن</label>
+            </div>
+            <p className="error">{errors.gender}</p>
           </div>
 
           <input
@@ -114,6 +189,7 @@ function App() {
             value={formData.birthDate}
             onChange={handleChange}
           />
+          <p className="error">{errors.birthDate}</p>
 
           <select
             name="country"
@@ -126,6 +202,7 @@ function App() {
             <option value="germany">آلمان</option>
             <option value="usa">آمریکا</option>
           </select>
+          <p className="error">{errors.country}</p>
 
           <textarea
             name="bio"
@@ -133,16 +210,20 @@ function App() {
             value={formData.bio}
             onChange={handleChange}
           />
+          <p className="error">{errors.bio}</p>
 
-          <div className="terms-container">
-            <input
-              type="checkbox"
-              name="terms"
-              checked={formData.terms}
-              onChange={handleChange}
-              className="checkbox"
-            />
-            <label>قوانین را می‌پذیرم.</label>
+          <div>
+            <div className="terms-container">
+              <input
+                type="checkbox"
+                name="terms"
+                checked={formData.terms}
+                onChange={handleChange}
+                className="checkbox"
+              />
+              <label>قوانین را می‌پذیرم.</label>
+            </div>
+            <p className="error">{errors.terms}</p>
           </div>
 
           <button type="submit">ثبت‌نام</button>
